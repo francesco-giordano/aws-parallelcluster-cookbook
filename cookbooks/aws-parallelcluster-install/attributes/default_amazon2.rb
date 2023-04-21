7da0e67160820033cac4faba2039d295e3c96483
Francesco Giordano <giordafr@amazon.it>
2023-04-21 14:52:45 +0200

    fix
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

--- cookbooks/aws-parallelcluster-install/attributes/default_amazon2.rb
+++ cookbooks/aws-parallelcluster-install/attributes/default_amazon2.rb
@@ -31,4 +31,3 @@ default['cluster']['armpl']['url'] = [
   node['cluster']['armpl']['platform'],
   "arm-performance-libraries_#{node['cluster']['armpl']['version']}_#{node['cluster']['armpl']['platform']}_gcc-#{node['cluster']['armpl']['gcc']['major_minor_version']}.tar",
 ].join('/')
-
